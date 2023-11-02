9580691ae59413ecdf454da425c24e3521b3d07b
3bdalah <3bdallah.dev@gmail.com>
2023-11-02 03:33:24 +0200

    feat(sidebar):completed sidebar component

---
+++ src/app/shared/sidebar/LinksDataRoutes.ts
@@ -0,0 +1,32 @@
+export const linksOverview = [
+  {
+    to: '',
+    text: 'home',
+    icone: 'fa fa-home',
+  },
+  {
+    to: 'dashboard',
+    text: 'dashboard',
+    icone: 'fa fa-dashboard',
+  },
+  {
+    to: 'manage',
+    text: 'manage',
+    icone: 'fa fa-book',
+  },
+  {
+    to: 'register',
+    text: 'register',
+    icone: 'fa fa-key',
+  },
+  {
+    to: 'jobs',
+    text: 'jobs',
+    icone: 'fa fa-clipboard-list',
+  },
+  {
+    to: 'staff',
+    text: 'staff',
+    icone: 'fa fa-users',
+  },
+];
